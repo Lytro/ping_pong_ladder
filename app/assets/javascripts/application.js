2077931b1984d7c7e34eb2dcf7d82e38db05106b
Anuj Biyani <abiyani@lytro.com>
2012-12-19 20:10:58 -0800

    unminify jquery switch to a global-bundle so application.js can be used for shared js

--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-//= require_self
-//= require jquery.min
-//= require raphael-min
-//= require jquery_ujs
-//= require jquery.tabs
-//= require jquery.autocomplete
-//= require twitter/bootstrap
-//= require initialize
---
+++ app/assets/javascripts/global-bundle.js
@@ -0,0 +1,8 @@
+//= require jquery-1.8.3
+//= require application
+//= require raphael-min
+//= require jquery_ujs
+//= require jquery.tabs
+//= require jquery.autocomplete
+//= require twitter/bootstrap
+//= require initialize
