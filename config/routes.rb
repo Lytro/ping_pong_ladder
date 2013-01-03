0e79f29fa6ca494ccb6c958257339c0d38136a12
Anuj Biyani <abiyani@lytro.com>
2013-01-02 19:32:58 -0800

    add initial set of rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,30 @@
 Pong::Application.routes.draw do
   resources :achievements, only: [:index]
+
   resources :matches do
     collection do
       get 'rankings'
       get 'players'
     end
   end
+
   resources :updates, only: [:index]
+
   resources :players, only: [:show, :update] do
     collection do
       get 'odds'
     end
   end
+
   resources :stats, only: [:index]
+
+  resources :rules, only: [:index]
+
   resource :api do
     member do
       get 'tweet'
     end
   end
+
   root to: 'matches#rankings'
 end
