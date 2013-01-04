5b0f4a4698627daa23b6996d3100a3c32c0f31cd
Anuj Biyani <abiyani@lytro.com>
2013-01-03 16:34:12 -0800

    fix the hipchat room name

--- config/app/production.rb
+++ config/app/production.rb
@@ -2,6 +2,6 @@ App.configure do
   # Settings specified here will take precedence over those in config/app.rb
 
   config.hipchat.update({
-    room: 'Ping pong buddies'
+    room: 'Ping pong buddies!'
   })
 end
